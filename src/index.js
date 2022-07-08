3ffcecf8712cb798fea90671f404d6f849e488bc
Mykhailo Shyhin <johnglagol@gmail.com>
2022-07-08 13:55:46 +0300

    done task

--- src/index.js
+++ src/index.js
@@ -4,29 +4,23 @@ import fetchCountries from './js/fetchCountries';
 const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
-const URL = 'https://restcountries.com/v3.1/name/';
-const filter = '?field=name,capital,population,flags,languages'
 const inputCountry = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
-let nameOfCountry = '';
 
 inputCountry.addEventListener('keyup',
     debounce((e) => {
-        nameOfCountry = e.target.value.trim();
-    
-        // if (nameOfCountry.length < 2) {
-        //     resetMarkup(countryInfo, countryList);
-        //     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-        //     return
-        // }
-      
+        let nameOfCountry = e.target.value.trim();
+        if (nameOfCountry.length === 0) {
+            resetMarkup(countryInfo, countryList)
+            return
+        }
 
         fetchCountries(nameOfCountry).then(data => {
         if (data.length === 1) {
             resetMarkup(countryInfo, countryList)
             markupInfo(data);
-        } if (data.length <= 10) {
+        } else if (data.length > 1 && data.length <= 10) {
             resetMarkup(countryInfo, countryList)
             markupList(data);            
         }
@@ -53,10 +47,10 @@ function resetMarkup(info, list) {
 
 function markupList(countries) {
    const contriesArray = countries.map(country => {
-       return  `<li>
+       return  `<li class="country-item">
             
-            <img src=${country.flags.png}>
-            <span>${country.name.official}</span>
+            <img src=${country.flags.png} width="80">
+            <span class="country-name">${country.name.official}</span>
         </li>`
     });
     contriesArray.forEach(markupCountry => {
@@ -69,11 +63,13 @@ function markupList(countries) {
 function markupInfo(countries) {
     country = countries[0];
     console.log(country.languages)
-    const info = `<p>${country.name.official}</p>
-            <img src=${country.flags.png}> 
-            <p>Capital: ${country.capital}<p>
-            <p>Population: ${country.population}</p>
-            <p>Languages: ${Object.values(country.languages).join(', ')}</p>`
+    const info = `<div class="country-info__inner">
+            <img src=${country.flags.png} width="50">
+            <span class="country-name__big">${country.name.official}</span>
+            </div>
+            <p class="country-text"><span class="country-info--accent">Capital:</span> ${country.capital}<p>
+            <p class="country-text"><span class="country-info--accent">Population:</span> ${country.population}</p>
+            <p class=country-text"><span class="country-info--accent">Languages:</span> ${Object.values(country.languages).join(', ')}</p>`
     
         countryInfo.insertAdjacentHTML('beforeend', info)
 };
